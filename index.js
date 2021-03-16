d3e5a60697ad239cb568de3b9ebcd5c4bd1815f4
thisismaw <iamwillempacardo@gmail.com>
2021-03-16 10:15:46 +0800

    fx

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 
 var generateRoll = document.querySelectorAll(".roll")[0].addEventListener("click", function (){
 
-    var diceRandom = Math.floor((Math.random()* 6)+1);
-    var diceSource = "dice-" + diceRandom + ".png";
-    var diceFolder = "images/" + diceSource;
-    var diceRandom2 = Math.floor((Math.random()* 6)+1);
-    var diceSource2 = "dice-" + diceRandom2 + ".png";
-    var diceFolder2 = "images/" + diceSource2;
-    document.querySelectorAll("img")[0].src= diceFolder;
-    document.querySelectorAll("img")[1].src= diceFolder2;
+    
+    for (x = 0; x < 3; x++) {
+        var diceRandom = Math.floor((Math.random()* 6)+1);
+        var diceSource = "dice-" + diceRandom + ".png";
+        var diceFolder = "images/" + diceSource;
+        var diceRandom2 = Math.floor((Math.random()* 6)+1);
+        var diceSource2 = "dice-" + diceRandom2 + ".png";
+        var diceFolder2 = "images/" + diceSource2;
+        document.querySelectorAll("img")[0].src= diceFolder;
+        document.querySelectorAll("img")[1].src= diceFolder2;
+        var audio = new Audio("soundfx/arcadealert.wav");
+        audio.play();
 
+    }
     if (diceRandom > diceRandom2)
     {
         document.querySelector("h2").innerHTML = "Player 1 Wins!";
-        var audio = new Audio("soundfx/arcadealert.wav");
-        audio.play();
+
+
     }
     else if (diceRandom < diceRandom2) {
         document.querySelector("h2").innerHTML = "Player 2 Wins!";
-        var audio = new Audio("soundfx/arcadealert.wav");
-        audio.play();
+
     }
     else {
         document.querySelector("h2").innerHTML = "It's A Tie!";
-        var audio = new Audio("soundfx/tiesound.wav");
-        audio.play();
+
     }
 })
